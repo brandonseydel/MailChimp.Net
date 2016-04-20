fd972795dd0cc1da0810e827750271452b28f645
Brandon Seydel <brandonseydel@hotmail.com>
2016-04-20 07:24:47 -0500

    Updated IMailchimpmanager to include all of the logic layers

--- MailChimp.Net/Interfaces/IMailChimpManager.cs
+++ MailChimp.Net/Interfaces/IMailChimpManager.cs
@@ -49,5 +49,121 @@ namespace MailChimp.Net.Interfaces
         /// Gets the members.
         /// </summary>
         INoteLogic Notes { get; }
+
+        /// <summary>
+        /// Gets the abuse reports.
+        /// </summary>
+        IAbuseReportLogic AbuseReports { get; }
+
+        /// <summary>
+        /// Gets the activities.
+        /// </summary>
+        IActivityLogic Activities { get; }
+
+        /// <summary>
+        /// Gets or sets the automation email queues.
+        /// </summary>
+        IAutomationEmailQueueLogic AutomationEmailQueues { get; }
+
+        /// <summary>
+        /// Gets or sets the automation emails.
+        /// </summary>
+        IAutomationEmailLogic AutomationEmails { get; }
+
+        /// <summary>
+        /// Gets or sets the automations.
+        /// </summary>
+        IAutomationLogic Automations { get; }
+
+        /// <summary>
+        /// Gets or sets the automation subscribers.
+        /// </summary>
+        IAutomationSubscriberLogic AutomationSubscribers { get; }
+
+        /// <summary>
+        /// Gets or sets the campaign folders.
+        /// </summary>
+        ICampaignFolderLogic CampaignFolders { get; }
+
+        /// <summary>
+        /// Gets the clients.
+        /// </summary>
+        IClientLogic Clients { get; }
+
+        /// <summary>
+        /// Gets or sets the e commerce stores.
+        /// </summary>
+        IECommerceLogic ECommerceStores { get; set; }
+
+        /// <summary>
+        /// Gets the feedback.
+        /// </summary>
+        IFeedbackLogic Feedback { get; }
+
+        /// <summary>
+        /// Gets the file manager files.
+        /// </summary>
+        IFileManagerFileLogic FileManagerFiles { get; }
+
+        /// <summary>
+        /// Gets the file manager folders.
+        /// </summary>
+        IFileManagerFolderLogic FileManagerFolders { get; }
+
+        /// <summary>
+        /// Gets the growth histories.
+        /// </summary>
+        IGrowthHistoryLogic GrowthHistories { get; }
+
+        /// <summary>
+        /// Gets the interest categories.
+        /// </summary>
+        IInterestCategoryLogic InterestCategories { get; }
+
+        /// <summary>
+        /// Gets or sets the interests.
+        /// </summary>
+        IInterestLogic Interests { get; }
+
+
+        /// <summary>
+        /// Gets the merge fields.
+        /// </summary>
+        IMergeFieldLogic MergeFields { get; }
+
+        /// <summary>
+        /// Gets or sets the messages.
+        /// </summary>
+        IMessageLogic Messages { get; }
+
+        /// <summary>
+        /// Gets or sets the reports.
+        /// </summary>
+        IReportLogic Reports { get; }
+
+        /// <summary>
+        /// Gets the segments.
+        /// </summary>
+        IListSegmentLogic Segments { get; }
+
+        /// <summary>
+        /// Gets the send checklists.
+        /// </summary>
+        ISendChecklist SendChecklists { get; }
+
+        /// <summary>
+        /// Gets or sets the template folders.
+        /// </summary>
+        ITemplateFolderLogic TemplateFolders { get; }
+
+        /// <summary>
+        /// Gets or sets the templates.
+        /// </summary>
+        ITemplateLogic Templates { get; }
+
+        /// <summary>
+        /// Gets the logic to access mail chimp web hooks
+        /// </summary>
+        IWebHookLogic WebHooks { get; set; }
     }
 }
