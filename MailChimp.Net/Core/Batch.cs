99f29006381362328c1534af72c94d01583ff1c0
Guy Haskell <software@manicfiddler.org>
2016-05-06 16:03:03 +0100

    Spelling correction

--- MailChimp.Net/Core/Batch.cs
+++ MailChimp.Net/Core/Batch.cs
@@ -7,55 +7,58 @@ using Newtonsoft.Json;
 
 namespace MailChimp.Net.Core
 {
-    public class Batch
-    {
-        /// <summary>
-        /// A string that uniquely identifies this batch request.
-        /// </summary>
-        public string Id { get; set; }
+	public class Batch
+	{
+		/// <summary>
+		/// A string that uniquely identifies this batch request.
+		/// </summary>
+		public string Id { get; set; }
 
-        /// <summary>
-        /// The status of the batch call.
-        /// Possible Values:
-        /// pending - started - finished
-        /// </summary>
-        public string Status { get; set; }
+		/// <summary>
+		/// The status of the batch call.
+		/// Possible Values:
+		/// pending - started - finished
+		/// </summary>
+		public string Status { get; set; }
 
-        /// <summary>
-        /// The total number of operations to complete as part of this batch request. 
-        /// For GET requests requiring pagination, each page counts as a separate operation.
-        /// </summary>
-        [JsonProperty("total_operations")]
-        public int TotalOperations { get; set; }
+		/// <summary>
+		/// The total number of operations to complete as part of this batch request. 
+		/// For GET requests requiring pagination, each page counts as a separate operation.
+		/// </summary>
+		[JsonProperty("total_operations")]
+		public int TotalOperations { get; set; }
 
-        /// <summary>
-        /// The number of completed operations. This includes operations that returned an error.
-        /// </summary>
-        [JsonProperty("finished_operations")]
-        public string FinishedOperpations { get; set; }
+		[System.ComponentModel.Browsable(false), Obsolete("Spelling corrected to FinishedOperations")]
+		public string FinishedOperpations { get; set; }
 
-        /// <summary>
-        /// The number of completed operations that returned an error.
-        /// </summary>
-        [JsonProperty("errored_operations")]
-        public string ErroredOperations { get; set; }
+		/// <summary>
+		/// The number of completed operations. This includes operations that returned an error.
+		/// </summary>
+		[JsonProperty("finished_operations")]
+		public string FinishedOperations { get; set; }
 
-        /// <summary>
-        /// The time and date when the server received the batch request.
-        /// </summary>
-        [JsonProperty("submitted_at")]
-        public string SubmittedAt { get; set; }
+		/// <summary>
+		/// The number of completed operations that returned an error.
+		/// </summary>
+		[JsonProperty("errored_operations")]
+		public string ErroredOperations { get; set; }
 
-        /// <summary>
-        /// The time and date when all operations in the batch request completed.
-        /// </summary>
-        [JsonProperty("completed_at")]
-        public string CompletedAt { get; set; }
+		/// <summary>
+		/// The time and date when the server received the batch request.
+		/// </summary>
+		[JsonProperty("submitted_at")]
+		public string SubmittedAt { get; set; }
 
-        /// <summary>
-        /// The URL of the gzipped archive of the results of all the operations.
-        /// </summary>
-        [JsonProperty("response_body_url")]
-        public string ResponseBodyUrl { get; set; }
-    }
+		/// <summary>
+		/// The time and date when all operations in the batch request completed.
+		/// </summary>
+		[JsonProperty("completed_at")]
+		public string CompletedAt { get; set; }
+
+		/// <summary>
+		/// The URL of the gzipped archive of the results of all the operations.
+		/// </summary>
+		[JsonProperty("response_body_url")]
+		public string ResponseBodyUrl { get; set; }
+	}
 }
