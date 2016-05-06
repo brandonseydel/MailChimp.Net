ec66b7915a7a56f115d5f305959cd3c44c15ba94
Guy Haskell <software@manicfiddler.org>
2016-05-06 15:58:39 +0100

    Add method to get status of specific batch

--- MailChimp.Net/Logic/BatchLogic.cs
+++ MailChimp.Net/Logic/BatchLogic.cs
@@ -13,63 +13,74 @@ using MailChimp.Net.Interfaces;
 
 namespace MailChimp.Net.Logic
 {
-    /// <summary>
-    /// The batch operation logic.
-    /// </summary>
-    internal class BatchLogic : BaseLogic, IBatchLogic
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="Logic.BatchLogic"/> class.
-        /// </summary>
-        /// <param name="apiKey">
-        /// The api key.
-        /// </param>
-        public BatchLogic(string apiKey) : base(apiKey)
-        {
-        }
+	/// <summary>
+	/// The batch operation logic.
+	/// </summary>
+	internal class BatchLogic : BaseLogic, IBatchLogic
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Logic.BatchLogic"/> class.
+		/// </summary>
+		/// <param name="apiKey">
+		/// The api key.
+		/// </param>
+		public BatchLogic(string apiKey) : base(apiKey)
+		{
+		}
 
-        public async Task<Batch> AddAsync(BatchRequest request = null)
-        {
-            using (var client = this.CreateMailClient("batches"))
-            {
-                var response =
-                    await
-                        client.PostAsJsonAsync(string.Empty, request)
-                            .ConfigureAwait(false);
-                await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
-                return await response.Content.ReadAsAsync<Batch>().ConfigureAwait(false);
-            }
-        }
+		public async Task<Batch> AddAsync(BatchRequest request = null)
+		{
+			using (var client = this.CreateMailClient("batches"))
+			{
+				var response =
+					await
+						client.PostAsJsonAsync(string.Empty, request)
+							.ConfigureAwait(false);
+				await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
+				return await response.Content.ReadAsAsync<Batch>().ConfigureAwait(false);
+			}
+		}
 
-        public async Task<IEnumerable<Batch>> GetAllAsync(QueryableBaseRequest request = null)
-        {
-            using (var client = this.CreateMailClient("batches"))
-            {
-                var response =
-                    await
-                        client.GetAsync(request?.ToQueryString() ?? string.Empty)
-                            .ConfigureAwait(false);
-                await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
-                var batchResponse = await response.Content.ReadAsAsync<BatchResponse>().ConfigureAwait(false);
-                return batchResponse.Batches;
-            }
-        }
+		public async Task<IEnumerable<Batch>> GetAllAsync(QueryableBaseRequest request = null)
+		{
+			using (var client = this.CreateMailClient("batches"))
+			{
+				var response =
+					await
+						client.GetAsync(request?.ToQueryString() ?? string.Empty)
+							.ConfigureAwait(false);
+				await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
+				var batchResponse = await response.Content.ReadAsAsync<BatchResponse>().ConfigureAwait(false);
+				return batchResponse.Batches;
+			}
+		}
 
-        public async Task<BatchResponse> GetResponseAsync(QueryableBaseRequest request = null)
-        {
-            using (var client = this.CreateMailClient("batches"))
-            {
-                var response =
-                    await
-                        client.GetAsync(request?.ToQueryString() ?? string.Empty)
-                            .ConfigureAwait(false);
-                await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
-                var batchResponse = await response.Content.ReadAsAsync<BatchResponse>().ConfigureAwait(false);
-                return batchResponse;
-            }
-        }
+		public async Task<BatchResponse> GetResponseAsync(QueryableBaseRequest request = null)
+		{
+			using (var client = this.CreateMailClient("batches"))
+			{
+				var response =
+					await
+						client.GetAsync(request?.ToQueryString() ?? string.Empty)
+							.ConfigureAwait(false);
+				await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
+				var batchResponse = await response.Content.ReadAsAsync<BatchResponse>().ConfigureAwait(false);
+				return batchResponse;
+			}
+		}
 
+		public async Task<Batch> GetBatchStatus(string batchId)
+		{
+			using (var client = this.CreateMailClient($"batches/{batchId}"))
+			{
+				var response =
+					await
+						client.GetAsync(string.Empty).ConfigureAwait(false);
+				await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
+				var batchStatus = await response.Content.ReadAsAsync<Batch>().ConfigureAwait(false);
+				return batchStatus;	
+			}
+		}
 
-
-    }
+	}
 }
