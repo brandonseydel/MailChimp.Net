43bc7a03fae9ec51dbf8ae244b8eb059b15569d2
Guy Haskell <software@manicfiddler.org>
2016-05-06 13:57:00 +0100

    Expose Hash through MemberLogic to ease batch operations including creating List Members as you need to provide the hash of the email address

--- MailChimp.Net/Interfaces/IMemberLogic.cs
+++ MailChimp.Net/Interfaces/IMemberLogic.cs
@@ -11,88 +11,90 @@ using MailChimp.Net.Models;
 
 namespace MailChimp.Net.Interfaces
 {
-    using System;
+	using System;
 
-    /// <summary>
-    /// The MemberLogic interface.
-    /// </summary>
-    public interface IMemberLogic
-    {
-        /// <summary>
-        /// The add or update async.
-        /// </summary>
-        /// <param name="listId">
-        /// The list id.
-        /// </param>
-        /// <param name="member">
-        /// The member.
-        /// </param>
-        /// <returns>
-        /// The <see cref="Task"/>.
-        /// </returns>
-        Task<Member> AddOrUpdateAsync(string listId, Member member);
+	/// <summary>
+	/// The MemberLogic interface.
+	/// </summary>
+	public interface IMemberLogic
+	{
+		/// <summary>
+		/// The add or update async.
+		/// </summary>
+		/// <param name="listId">
+		/// The list id.
+		/// </param>
+		/// <param name="member">
+		/// The member.
+		/// </param>
+		/// <returns>
+		/// The <see cref="Task"/>.
+		/// </returns>
+		Task<Member> AddOrUpdateAsync(string listId, Member member);
 
-        /// <summary>
-        /// The delete async.
-        /// </summary>
-        /// <param name="listId">
-        /// The list id.
-        /// </param>
-        /// <param name="emailAddress">
-        /// The email address.
-        /// </param>
-        /// <returns>
-        /// The <see cref="Task"/>.
-        /// </returns>
-        Task DeleteAsync(string listId, string emailAddress);
+		/// <summary>
+		/// The delete async.
+		/// </summary>
+		/// <param name="listId">
+		/// The list id.
+		/// </param>
+		/// <param name="emailAddress">
+		/// The email address.
+		/// </param>
+		/// <returns>
+		/// The <see cref="Task"/>.
+		/// </returns>
+		Task DeleteAsync(string listId, string emailAddress);
 
-        /// <summary>
-        /// The get all async.
-        /// </summary>
-        /// <param name="listId">
-        /// The list id.
-        /// </param>
-        /// <param name="memberRequest"></param>
-        /// <returns>
-        /// The <see cref="Task"/>.
-        /// </returns>
-        Task<IEnumerable<Member>> GetAllAsync(string listId, MemberRequest memberRequest = null);
+		/// <summary>
+		/// The get all async.
+		/// </summary>
+		/// <param name="listId">
+		/// The list id.
+		/// </param>
+		/// <param name="memberRequest"></param>
+		/// <returns>
+		/// The <see cref="Task"/>.
+		/// </returns>
+		Task<IEnumerable<Member>> GetAllAsync(string listId, MemberRequest memberRequest = null);
 
-        /// <summary>
-        /// Get the total number of members in the list
-        /// </summary>
-        /// <param name="listId"></param>
-        /// <param name="status"></param>
-        /// <returns></returns>
-        Task<int> GetTotalItems(string listId, Status? status);
+		/// <summary>
+		/// Get the total number of members in the list
+		/// </summary>
+		/// <param name="listId"></param>
+		/// <param name="status"></param>
+		/// <returns></returns>
+		Task<int> GetTotalItems(string listId, Status? status);
 
-        /// <summary>
-        /// The get async.
-        /// </summary>
-        /// <param name="listId">
-        /// The list id.
-        /// </param>
-        /// <param name="emailAddress">
-        /// The email address.
-        /// </param>
-        /// <param name="request"></param>
-        /// <returns>
-        /// The <see cref="Task"/>.
-        /// </returns>
-        Task<Member> GetAsync(string listId, string emailAddress, BaseRequest request = null);
+		/// <summary>
+		/// The get async.
+		/// </summary>
+		/// <param name="listId">
+		/// The list id.
+		/// </param>
+		/// <param name="emailAddress">
+		/// The email address.
+		/// </param>
+		/// <param name="request"></param>
+		/// <returns>
+		/// The <see cref="Task"/>.
+		/// </returns>
+		Task<Member> GetAsync(string listId, string emailAddress, BaseRequest request = null);
 
-        /// <exception cref="ArgumentNullException"><paramref>
-        ///         <name>uriString</name>
-        ///     </paramref>
-        ///     is null. </exception>
-        /// <exception cref="UriFormatException">In the .NET for Windows Store apps or the Portable Class Library, catch the base class exception, <see cref="T:System.FormatException" />, instead.<paramref name="uriString" /> is empty.-or- The scheme specified in <paramref name="uriString" /> is not correctly formed. See <see cref="M:System.Uri.CheckSchemeName(System.String)" />.-or- <paramref name="uriString" /> contains too many slashes.-or- The password specified in <paramref name="uriString" /> is not valid.-or- The host name specified in <paramref name="uriString" /> is not valid.-or- The file name specified in <paramref name="uriString" /> is not valid. -or- The user name specified in <paramref name="uriString" /> is not valid.-or- The host or authority name specified in <paramref name="uriString" /> cannot be terminated by backslashes.-or- The port number specified in <paramref name="uriString" /> is not valid or cannot be parsed.-or- The length of <paramref name="uriString" /> exceeds 65519 characters.-or- The length of the scheme specified in <paramref name="uriString" /> exceeds 1023 characters.-or- There is an invalid character sequence in <paramref name="uriString" />.-or- The MS-DOS path specified in <paramref name="uriString" /> must start with c:\\.</exception>
-        /// <exception cref="InvalidOperationException">This member belongs to a type that is loaded into the reflection-only context. See How to: Load Assemblies into the Reflection-Only Context.</exception>
-        /// <exception cref="MailChimpException">
-        /// Custom Mail Chimp Exception
-        /// </exception>
-        /// <exception cref="NotSupportedException"><paramref name="element" /> is not a constructor, method, property, event, type, or field. </exception>
-        /// <exception cref="TypeLoadException">A custom attribute type cannot be loaded. </exception>
-        /// <exception cref="ArgumentOutOfRangeException">Enlarging the value of this instance would exceed <see cref="P:System.Text.StringBuilder.MaxCapacity" />. </exception>
-        Task<MemberResponse> GetResponseAsync(string listId, MemberRequest memberRequest = null);
-    }
+		/// <exception cref="ArgumentNullException"><paramref>
+		///         <name>uriString</name>
+		///     </paramref>
+		///     is null. </exception>
+		/// <exception cref="UriFormatException">In the .NET for Windows Store apps or the Portable Class Library, catch the base class exception, <see cref="T:System.FormatException" />, instead.<paramref name="uriString" /> is empty.-or- The scheme specified in <paramref name="uriString" /> is not correctly formed. See <see cref="M:System.Uri.CheckSchemeName(System.String)" />.-or- <paramref name="uriString" /> contains too many slashes.-or- The password specified in <paramref name="uriString" /> is not valid.-or- The host name specified in <paramref name="uriString" /> is not valid.-or- The file name specified in <paramref name="uriString" /> is not valid. -or- The user name specified in <paramref name="uriString" /> is not valid.-or- The host or authority name specified in <paramref name="uriString" /> cannot be terminated by backslashes.-or- The port number specified in <paramref name="uriString" /> is not valid or cannot be parsed.-or- The length of <paramref name="uriString" /> exceeds 65519 characters.-or- The length of the scheme specified in <paramref name="uriString" /> exceeds 1023 characters.-or- There is an invalid character sequence in <paramref name="uriString" />.-or- The MS-DOS path specified in <paramref name="uriString" /> must start with c:\\.</exception>
+		/// <exception cref="InvalidOperationException">This member belongs to a type that is loaded into the reflection-only context. See How to: Load Assemblies into the Reflection-Only Context.</exception>
+		/// <exception cref="MailChimpException">
+		/// Custom Mail Chimp Exception
+		/// </exception>
+		/// <exception cref="NotSupportedException"><paramref name="element" /> is not a constructor, method, property, event, type, or field. </exception>
+		/// <exception cref="TypeLoadException">A custom attribute type cannot be loaded. </exception>
+		/// <exception cref="ArgumentOutOfRangeException">Enlarging the value of this instance would exceed <see cref="P:System.Text.StringBuilder.MaxCapacity" />. </exception>
+		Task<MemberResponse> GetResponseAsync(string listId, MemberRequest memberRequest = null);
+
+		string Hash(string emailAddress);
+	}
 }
