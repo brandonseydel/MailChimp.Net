5c21d3502cc841d5e8d2a951fced253f4f587901
manicfiddler <software@manicfiddler.org>
2016-05-10 01:23:05 +0100

    Default values for various model properties to ease client code (#22)

--- MailChimp.Net/Models/Location.cs
+++ MailChimp.Net/Models/Location.cs
@@ -8,45 +8,50 @@ using Newtonsoft.Json;
 
 namespace MailChimp.Net.Models
 {
-    /// <summary>
-    /// The location.
-    /// </summary>
-    public class Location
-    {
-        /// <summary>
-        /// Gets or sets the country code.
-        /// </summary>
-        [JsonProperty("country_code")]
-        public string CountryCode { get; set; }
+	/// <summary>
+	/// The location.
+	/// </summary>
+	public class Location
+	{
+		public Location()
+		{
+			CountryCode = string.Empty;
+			Timezone = string.Empty;
+		}
+		/// <summary>
+		/// Gets or sets the country code.
+		/// </summary>
+		[JsonProperty("country_code")]
+		public string CountryCode { get; set; }
 
-        /// <summary>
-        /// Gets or sets the dstoff.
-        /// </summary>
-        [JsonProperty("dstoff")]
-        public int Dstoff { get; set; }
+		/// <summary>
+		/// Gets or sets the dstoff.
+		/// </summary>
+		[JsonProperty("dstoff")]
+		public int Dstoff { get; set; }
 
-        /// <summary>
-        /// Gets or sets the gmtoff.
-        /// </summary>
-        [JsonProperty("gmtoff")]
-        public int Gmtoff { get; set; }
+		/// <summary>
+		/// Gets or sets the gmtoff.
+		/// </summary>
+		[JsonProperty("gmtoff")]
+		public int Gmtoff { get; set; }
 
-        /// <summary>
-        /// Gets or sets the latitude.
-        /// </summary>
-        [JsonProperty("latitude")]
-        public decimal Latitude { get; set; }
+		/// <summary>
+		/// Gets or sets the latitude.
+		/// </summary>
+		[JsonProperty("latitude")]
+		public decimal Latitude { get; set; }
 
-        /// <summary>
-        /// Gets or sets the longitude.
-        /// </summary>
-        [JsonProperty("longitude")]
-        public decimal Longitude { get; set; }
+		/// <summary>
+		/// Gets or sets the longitude.
+		/// </summary>
+		[JsonProperty("longitude")]
+		public decimal Longitude { get; set; }
 
-        /// <summary>
-        /// Gets or sets the timezone.
-        /// </summary>
-        [JsonProperty("timezone")]
-        public string Timezone { get; set; }
-    }
+		/// <summary>
+		/// Gets or sets the timezone.
+		/// </summary>
+		[JsonProperty("timezone")]
+		public string Timezone { get; set; }
+	}
 }
