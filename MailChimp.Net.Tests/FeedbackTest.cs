6e32221acef2458ec7a02b63499600a7f594a73b
Cristoffer Andersson <cristoffer.andersson@gmail.com>
2016-10-23 16:18:34 +0200

    Add tests for creating, fetching and deleting feedback on a campaign

--- MailChimp.Net.Tests/FeedbackTest.cs
+++ MailChimp.Net.Tests/FeedbackTest.cs
@@ -7,6 +7,10 @@
 using System;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MailChimp.Net.Models;
+using MailChimp.Net.Core;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace MailChimp.Net.Tests
 {
@@ -16,8 +20,106 @@ namespace MailChimp.Net.Tests
     [TestClass]
     public class FeedbackTest : MailChimpTest
     {
-        
-        
+        /// <summary>
+        /// Should_Create_One_Feedback.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        [TestMethod]
+        public async Task Should_Create_One_Feedback()
+        {
+            await this.ClearMailChimpAsync().ConfigureAwait(true);
 
+            var campaign = await CreateCampaign();
+            
+            var createdFeedback = await this._mailChimpManager.Feedback.AddOrUpdateAsync(campaign.Id, new Feedback() {
+                Message = "Test feedback"
+            });
+
+            var feedback = await this._mailChimpManager.Feedback.GetAsync(campaign.Id, createdFeedback.FeedbackId.Value.ToString());
+
+            Assert.IsNotNull(feedback);
+        }
+
+        /// <summary>
+        /// Should_Return_Feedback.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        [TestMethod]
+        public async Task Should_Return_Feedback()
+        {
+            var campaign = await CreateCampaign();
+            var feedbacks = await this._mailChimpManager.Feedback.GetAllAsync(campaign.Id);
+            Assert.IsNotNull(feedbacks);
+        }
+
+        /// <summary>
+        /// Should_Return_Multiple_Feedback.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        [TestMethod]
+        public async Task Should_Return_Multiple_Feedback()
+        {
+            await this.ClearMailChimpAsync().ConfigureAwait(true);
+
+            var campaign = await CreateCampaign();
+
+            for (int i = 0; i < 3; i++)
+            {
+                await this._mailChimpManager.Feedback.AddOrUpdateAsync(campaign.Id, new Feedback()
+                {
+                    Message = "Test feedback"
+                });
+            }
+
+            var feedbacks = await this._mailChimpManager.Feedback.GetAllAsync(campaign.Id);
+            Assert.IsTrue(feedbacks.Count() == 3);
+        }
+
+        /// <summary>
+        /// Should_Return_No_Feedback_After_Removal.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        [TestMethod]
+        public async Task Should_Return_No_Feedback_After_Removal()
+        {
+            await this.ClearMailChimpAsync().ConfigureAwait(true);
+
+            var campaign = await CreateCampaign();
+
+            var createdFeedback = await this._mailChimpManager.Feedback.AddOrUpdateAsync(campaign.Id, new Feedback()
+            {
+                Message = "Test feedback"
+            });
+
+            await this._mailChimpManager.Feedback.DeleteAsync(campaign.Id, createdFeedback.FeedbackId.Value.ToString());
+            var feedback = await this._mailChimpManager.Feedback.GetAllAsync(campaign.Id);
+
+            Assert.AreEqual(0, feedback.Count());
+        }
+
+        private async Task<Campaign> CreateCampaign()
+        {
+            var campaign = await this._mailChimpManager.Campaigns.AddAsync(new Campaign
+            {
+                Settings = new Setting
+                {
+                    ReplyTo = "test@test.com",
+                    Title = "Test Campaign",
+                    FromName = "TESTER",
+                    SubjectLine = "TEST"
+                },
+                Type = CampaignType.Plaintext,
+
+            }).ConfigureAwait(false);
+            return campaign;
+        }
     }
 }
