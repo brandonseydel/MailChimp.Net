263f059b76d3067c8f62b0951f9f2b4620ebde72
Brandon's Surface <Brandon's Surface@DESKTOP-VG4Q4RJ>
2016-11-16 22:15:57 -0600

    Updated Email Action to allow none

--- MailChimp.Net/Core/EmailAction.cs
+++ MailChimp.Net/Core/EmailAction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace MailChimp.Net.Core
     /// </summary>
     public enum EmailAction
     {
+        [Description("")]
+        None,
         Open,
         Click,
         Bounce
