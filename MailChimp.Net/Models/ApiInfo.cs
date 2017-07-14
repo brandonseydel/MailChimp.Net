3e4456373fbd46c7fa84db1d38b73e5c5d89ba73
Brandon's Surface <Brandon's Surface@DESKTOP-VG4Q4RJ>
2017-07-13 21:38:55 -0500

    -Added additional api info details

--- MailChimp.Net/Models/ApiInfo.cs
+++ MailChimp.Net/Models/ApiInfo.cs
@@ -19,6 +19,34 @@ namespace MailChimp.Net.Models
         [JsonProperty("account_id")]
         public string AccountId { get; set; }
 
+        [JsonProperty("login_id")]
+        public string LoginId { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+        [JsonProperty("username")]
+        public string UserName { get; set; }
+        [JsonProperty("avatar_url")]
+        public string AvatarUrl { get; set; }
+        [JsonProperty("role")]
+        public string Role { get; set; }
+        [JsonProperty("member_since")]
+        public string MemberSince { get; set; }
+        [JsonProperty("pricing_plan_type")]
+        public string PricingPlanType { get; set; }
+        [JsonProperty("first_payment")]
+        public string FirstPayment { get; set; }
+        [JsonProperty("account_timezone")]
+        public string AccountTimezone { get; set; }
+        [JsonProperty("account_industry")]
+        public string AccountIndustry { get; set; }
+        [JsonProperty("pro_enabled")]
+        public bool ProEnabled { get; set; }
+
         /// <summary>
         /// Gets or sets the account name.
         /// </summary>
@@ -36,6 +64,8 @@ namespace MailChimp.Net.Models
         /// </summary>
         [JsonProperty("last_login")]
         public string LastLogin { get; set; }
+
+
 
         /// <summary>
         /// Gets or sets the links.
