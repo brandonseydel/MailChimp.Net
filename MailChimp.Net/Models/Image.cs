38c85913998775306acdb3440447adb43c08f237
Terry Mitchell <terryjmitchell@gmail.com>
2017-12-02 21:45:39 +1100

    Add missing images collection on product

---
+++ MailChimp.Net/Models/Image.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MailChimp.Net.Models
+{
+    public class Image
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("url")]
+        public IList<string> VariantIds { get; set; }
+
+        [JsonProperty("_links")]
+        public IList<Link> Links { get; set; }
+    }
+}
