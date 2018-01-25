ed33905a74b4d5b4dca53b6e25b3a2652612c53b
Justin Poonwah <Justin@Symend.com>
2018-01-25 15:56:37 -0700

    Added the VariateSettings and Combination objects (Although I didn't fill them out completely) so I could get Click and Open reports for Combo Campaigns. These changes are useful if you're creating AB tests and need to get child campaigns of campaigns.

---
+++ MailChimp.Net/Models/VariateSettings.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MailChimp.Net.Models
+{
+    public class VariateSettings
+    {
+        [JsonProperty("winning_combination_id")]
+        public string WinningCombinationId { get; set; }
+
+        [JsonProperty("winning_campaign_id")]
+        public string WinningCampaignId { get; set; }
+        [JsonProperty("winner_criteria")]
+        public string WinnerCriteria { get; set; }
+        [JsonProperty("combinations")]
+        public Combination[] Combinations { get; set; }
+    }
+}
