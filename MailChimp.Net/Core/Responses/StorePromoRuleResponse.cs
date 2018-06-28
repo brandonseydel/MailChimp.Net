46832dffaa62b01166954f57355b45274f46803d
tom-wiles <40457930+tom-wiles@users.noreply.github.com>
2018-06-28 15:35:34 +0100

    - Added support for promo rules and codes (#311)

---
+++ MailChimp.Net/Core/Responses/StorePromoRuleResponse.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using MailChimp.Net.Models;
+using Newtonsoft.Json;
+
+namespace MailChimp.Net.Core
+{
+    public class StorePromoRuleResponse : BaseResponse
+    {
+
+        public StorePromoRuleResponse()
+        {
+            PromoRules = new List<PromoRule>();
+        }
+
+        [JsonProperty("store_id")]
+        public string StoreId { get; set; }
+
+        [JsonProperty("promo_rules")]
+        public IList<PromoRule> PromoRules { get; set; }
+    }
+}
