f37db0c4c479b381d41467cc3c0f634c7acf0be6
Adrian von Allmen <a.vonallmen@telaran.org>
2018-10-09 21:49:55 +0200

    Template and TemplateRequest Update
    Added missing TemplateRequest Fields.
    Adjust interface to follow MailChimp API documentation.
    Added initial steps of TemplateTests

--- MailChimp.Net/Logic/TemplateLogic.cs
+++ MailChimp.Net/Logic/TemplateLogic.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="TemplateLogic.cs" company="Brandon Seydel">
 //   N/A
 // </copyright>
@@ -53,11 +53,39 @@ namespace MailChimp.Net.Logic
             }
         }
 
-        public async Task<Template> CreateAsync(string name, string folderId, string html)
+        /// <summary>
+        /// The delete async.
+        /// </summary>
+        /// <param name="templateId">
+        /// The template id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref>
+        ///         <name>uriString</name>
+        ///     </paramref>
+        ///     is null. </exception>
+        /// <exception cref="ArgumentNullException">The <paramref name="requestUri" /> was null.</exception>
+        /// <exception cref="MailChimpException">
+        /// Custom Mail Chimp Exception
+        /// </exception>
+        /// <exception cref="UriFormatException">In the .NET for Windows Store apps or the Portable Class Library, catch the base class exception, <see cref="T:System.FormatException" />, instead.<paramref name="uriString" /> is empty.-or- The scheme specified in <paramref name="uriString" /> is not correctly formed. See <see cref="M:System.Uri.CheckSchemeName(System.String)" />.-or- <paramref name="uriString" /> contains too many slashes.-or- The password specified in <paramref name="uriString" /> is not valid.-or- The host name specified in <paramref name="uriString" /> is not valid.-or- The file name specified in <paramref name="uriString" /> is not valid. -or- The user name specified in <paramref name="uriString" /> is not valid.-or- The host or authority name specified in <paramref name="uriString" /> cannot be terminated by backslashes.-or- The port number specified in <paramref name="uriString" /> is not valid or cannot be parsed.-or- The length of <paramref name="uriString" /> exceeds 65519 characters.-or- The length of the scheme specified in <paramref name="uriString" /> exceeds 1023 characters.-or- There is an invalid character sequence in <paramref name="uriString" />.-or- The MS-DOS path specified in <paramref name="uriString" /> must start with c:\\.</exception>
+        /// <exception cref="InvalidOperationException">The request message was already sent by the <see cref="T:System.Net.Http.HttpClient" /> instance.</exception>
+        public async Task DeleteAsync(int templateId)
         {
             using (var client = CreateMailClient("templates/"))
             {
-                var response = await client.PostAsJsonAsync("", new { name, folder_id = folderId, html }).ConfigureAwait(false);
+                var response = await client.DeleteAsync($"{templateId}").ConfigureAwait(false);
+                await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
+            }
+        }
+
+        public async Task<Template> CreateAsync(string name, string folderId, string html)
+        {
+            using (var client = CreateMailClient("templates"))
+            {
+                var response = await client.PostAsJsonAsync(null, new { name, folder_id = folderId, html }).ConfigureAwait(false);
                 await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
                 return await response.Content.ReadAsAsync<Template>().ConfigureAwait(false);
             }
@@ -68,12 +96,22 @@ namespace MailChimp.Net.Logic
             using (var client = CreateMailClient("templates/"))
             {
                 var response =
-                    await client.PatchAsJsonAsync(templateId, new {name, folder_id = folderId, html}).ConfigureAwait(false);
+                    await client.PatchAsJsonAsync(templateId, new { name, folder_id = folderId, html }).ConfigureAwait(false);
                 await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
                 return await response.Content.ReadAsAsync<Template>().ConfigureAwait(false);
             }
         }
 
+        public async Task<Template> UpdateAsync(int templateId, string name, string folderId, string html)
+        {
+            using (var client = CreateMailClient("templates/"))
+            {
+                var response =
+                    await client.PatchAsJsonAsync(templateId.ToString(), new { name, folder_id = folderId, html }).ConfigureAwait(false);
+                await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
+                return await response.Content.ReadAsAsync<Template>().ConfigureAwait(false);
+            }
+        }
 
 
         public async Task<object> GetDefaultContentAsync(string templateId, BaseRequest request = null)
@@ -163,6 +201,34 @@ namespace MailChimp.Net.Logic
         /// Custom Mail Chimp Exception
         /// </exception>
         public async Task<Template> GetAsync(string templateId)
+        {
+            using (var client = CreateMailClient("templates/"))
+            {
+                var response = await client.GetAsync($"{templateId}").ConfigureAwait(false);
+                await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
+
+                return await response.Content.ReadAsAsync<Template>().ConfigureAwait(false);
+            }
+        }
+
+        /// <summary>
+        /// The get async.
+        /// </summary>
+        /// <param name="templateId">
+        /// The template id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException"><paramref>
+        ///         <name>uriString</name>
+        ///     </paramref>
+        ///     is null. </exception>
+        /// <exception cref="UriFormatException">In the .NET for Windows Store apps or the Portable Class Library, catch the base class exception, <see cref="T:System.FormatException" />, instead.<paramref name="uriString" /> is empty.-or- The scheme specified in <paramref name="uriString" /> is not correctly formed. See <see cref="M:System.Uri.CheckSchemeName(System.String)" />.-or- <paramref name="uriString" /> contains too many slashes.-or- The password specified in <paramref name="uriString" /> is not valid.-or- The host name specified in <paramref name="uriString" /> is not valid.-or- The file name specified in <paramref name="uriString" /> is not valid. -or- The user name specified in <paramref name="uriString" /> is not valid.-or- The host or authority name specified in <paramref name="uriString" /> cannot be terminated by backslashes.-or- The port number specified in <paramref name="uriString" /> is not valid or cannot be parsed.-or- The length of <paramref name="uriString" /> exceeds 65519 characters.-or- The length of the scheme specified in <paramref name="uriString" /> exceeds 1023 characters.-or- There is an invalid character sequence in <paramref name="uriString" />.-or- The MS-DOS path specified in <paramref name="uriString" /> must start with c:\\.</exception>
+        /// <exception cref="MailChimpException">
+        /// Custom Mail Chimp Exception
+        /// </exception>
+        public async Task<Template> GetAsync(int templateId)
         {
             using (var client = CreateMailClient("templates/"))
             {
