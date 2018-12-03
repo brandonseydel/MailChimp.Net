c94e8daa38271200d0739760e4908c2a889bb4b3
Jesper Eiby <jei@vertica.dk>
2018-12-03 16:39:49 +0100

    add MailchimpException test case

---
+++ MailChimp.Net.Tests/ExceptionTest.cs
@@ -0,0 +1,22 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using MailChimp.Net.Core;
+using MailChimp.Net.Models;
+using Xunit;
+
+namespace MailChimp.Net.Tests
+{
+    public class ExceptionTest : MailChimpTest
+    {
+        [Fact]
+        public void Should_Return_Same_Data_On_Multiple_Calls()
+        {
+            var exception = new MailChimpException(new MailChimpApiError(), new HttpResponseMessage());
+
+            var data1 = exception.Data;
+            var data2 = exception.Data;
+
+            Assert.Equal(data1, data2);
+        }
+    }
+}
