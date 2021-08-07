bc6b193dda9cd0f5f3f56ef7296ec54920de5634
Lance Robinson <35983326+lrob7325@users.noreply.github.com>
2021-08-07 19:08:33 -0400

    Added "tags-search" api
    The ability to search for Tags by list id was added. An array of ListTag is returned in the response. The ListTag class consists of the "Id" and "Name" properties.

---
+++ MailChimp.Net/Logic/TagsLogic.cs
@@ -0,0 +1,43 @@
+﻿using MailChimp.Net.Core;
+using MailChimp.Net.Interfaces;
+using MailChimp.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace MailChimp.Net.Logic
+{
+    internal class TagsLogic : BaseLogic, ITagsLogic
+    {
+        private const string BaseUrl = "lists";
+        public TagsLogic(MailChimpOptions mailChimpConfiguration)
+            : base(mailChimpConfiguration)
+        {
+        }
+
+        public async Task<IEnumerable<ListTag>> GetAllAsync(string listId, TagsRequest request = null)
+        {
+            return (await GetResponseAsync(listId, request).ConfigureAwait(false))?.Tags;
+        }
+
+        public async Task<ListTagsResponse> GetResponseAsync(string listId, TagsRequest request = null)
+        {
+            request = request ?? new TagsRequest
+            {
+                Limit = _limit
+            };
+
+            using (var client = CreateMailClient($"{BaseUrl}/"))
+            {
+                var response = await client.GetAsync($"{listId}/tag-search").ConfigureAwait(false);
+                await response.EnsureSuccessMailChimpAsync().ConfigureAwait(false);
+
+                var listTagsResponse = await response.Content.ReadAsAsync<ListTagsResponse>().ConfigureAwait(false);
+                return listTagsResponse;
+            }
+        }
+    }
+}
