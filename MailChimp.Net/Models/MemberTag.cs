49709ae8b6a8b63d0b54d3a8c8ba26c90388d2b8
Brandon Seydel <brandonseydel@hotmail.com>
2023-05-15 08:18:54 -0500

    closes #597

--- MailChimp.Net/Models/MemberTag.cs
+++ MailChimp.Net/Models/MemberTag.cs
@@ -4,6 +4,7 @@
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 
 using MailChimp.Net.Core;
@@ -12,20 +13,32 @@ using Newtonsoft.Json;
 
 namespace MailChimp.Net.Models;
 
-	/// <summary>
-	/// The tag for a member
-	/// </summary>
-	public class MemberTag
-	{
-		/// <summary>
-		/// Gets or sets the identifier.
-		/// </summary>
-		[JsonProperty("id")]
-		public int Id { get; set; }
+/// <summary>
+/// The tag for a member
+/// </summary>
+public class MemberTag
+{
+    /// <summary>
+    /// Gets or sets the identifier.
+    /// </summary>
+    [JsonProperty("id")]
+    public int Id { get; set; }
 
-		/// <summary>
-		/// Gets or sets the tag's name.
-		/// </summary>
-		[JsonProperty("name")]
-		public string Name { get; set; }
-	}
+    /// <summary>
+    /// Gets or sets the tag's name.
+    /// </summary>
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// The date and time the tag was added to the list member in ISO 8601 format.
+    /// </summary>
+    [JsonProperty("date_added")]
+    public DateTime? DateAdded { get; set; }
+
+    /// <summary>
+    /// The status for the tag on the member, pass in active to add a tag or inactive to remove it. Possible values: "inactive" or "active".
+    /// </summary>
+    [JsonProperty("status")]
+    public string Status { get; set; }
+}
